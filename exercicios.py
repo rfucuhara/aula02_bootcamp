f4a9545c26aaa10e9d9d9d4ebd6f08816b63a80a
rfucuhara <rfucuhara1401@gmail.com>
2024-10-24 21:53:51 -0300

    Resolvendo um exercicio

--- exercicios.py
+++ exercicios.py
@@ -20,6 +20,9 @@
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+data_do_usuario = "20/12/2022"
+lista = data_do_usuario.split("/")
+print(lista)
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
 # #### Booleanos (`bool`)
